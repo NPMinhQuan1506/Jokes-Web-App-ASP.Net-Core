fe89b00d300cb9b1af67d000f63352a3888f787c
npminhquan <npminhquan1506@gmail.com>
2022-04-24 21:47:37 +0700

    Inital Web App MVC

---
+++ JokeWebAppMVC/JokeWebAppMVC/Data/JokeWebAppMVCContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokeWebAppMVC.Models;
+
+namespace JokeWebAppMVC.Data
+{
+    public class JokeWebAppMVCContext : DbContext
+    {
+        public JokeWebAppMVCContext (DbContextOptions<JokeWebAppMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokeWebAppMVC.Models.Joke> Joke { get; set; }
+    }
+}
